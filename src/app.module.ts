29bedf16a4ef68e923017989e43e0859eea7d6a1
unknown <dejota0712@gmail.com>
2022-06-06 20:38:29 -0500

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,29 +7,29 @@ import { ActorModule } from './components/actor/actor.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+   // TypeOrmModule.forRoot({
+    //  type: 'postgres',
+      //host: 'localhost',
+     // port: 5432,
+     // username: 'postgres',
+      //password: 'jorge1993',
+     // database: 'testorm',
+     // entities: ['dist/entities/**/*.js'],
+      //synchronize: false,
+    //}),
+
+     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'ec2-3-226-163-72.compute-1.amazonaws.com',
       port: 5432,
-      username: 'postgres',
-      password: 'jorge1993',
-      database: 'testorm',
+      username: 'dmbgsnujobpbbm',
+      password:
+     'aa928019d60c01e7e235568553cb0850b30b367c8ae9026c4a86ea076e51ebce',
+     database: 'dc1bshqlq88th',
+     logging: false,
       entities: ['dist/entities/**/*.js'],
       synchronize: false,
-    }),
-
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'ec2-3-211-221-xxxxxx',
-    //   port: 5432,
-    //   username: 'xxxxxxx',
-    //   password:
-    //     'xxxxxxxxx',
-    //   database: 'XXXXX',
-    //   logging: false,
-    //   entities: ['dist/entities/**/*.js'],
-    //   synchronize: false,
-    // }),
+     }),
 
     CountryModule,
 
